f845d54ba2814b6fa97baf90850bf1c7ba3002af
Alex Feyerke <espy@users.noreply.github.com>
2019-03-15 18:32:03 +0100

    feat: waveform loader

--- assets/main.js
+++ assets/main.js
@@ -204,6 +204,8 @@ document.getElementById('transcript').addEventListener('keyup', function(){
   localStorage.setItem(fileName, this.value)
 }, false);
 
+/*
 window.onbeforeunload = function () {
   return 'Please make sure you’ve saved your work before leaving this page'
 }
+*/
